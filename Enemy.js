08862971d93a3583a0f351ddf2280e4bf2013284
Benjamin Wieczorek <cyfer@cyfernet.de>
2018-08-29 01:52:34 +0200

    Comic Sprites

--- Enemy.js
+++ Enemy.js
@@ -31,12 +31,14 @@ class Enemy extends PIXI.Sprite {
             if (this.x < 0) {
                 this.x = 0;
                 this.vx *= -1;
+                this.vxBase *= -1;
                 this.rotation = Math.atan2(this.vy, this.vx) - Math.PI/2;
             }
 
             if (this.x + this.width > arcInc.pixiApp.screen.width/arcInc.pixiApp.stage.scale.x) {
                 this.x = arcInc.pixiApp.screen.width/arcInc.pixiApp.stage.scale.x - this.width;
                 this.vx *= -1;
+                this.vxBase *= -1;
                 this.rotation = Math.atan2(this.vy, this.vx) - Math.PI/2;
             }
         }
