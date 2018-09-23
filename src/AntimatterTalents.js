4aed3fdccd1e5ad17acba638a21ea136f2bb7073
Benjamin Wieczorek <cyfer@cyfernet.de>
2018-09-23 22:04:17 +0200

    Made purity upgrades more frequent, power level unchanged.

--- src/AntimatterTalents.js
+++ src/AntimatterTalents.js
@@ -54,7 +54,7 @@ class AntimatterTalents {
             "refinerPurity": {
                 "title": "Refiner Purity",
                 "cost": 1e15,
-                "growthFactor": 850,
+                "growthFactor": 425,
                 "description": "Increases the purity of the Refined Antimatter. The purer, the more power can be harnessed.",
                 "effectTemplate": "{EFFECT} times purer"
             },
@@ -76,7 +76,7 @@ class AntimatterTalents {
         this.refinerBufferVolume = 1e15 * 10**(2 * arcInc.savegame.talents.refinerBufferVolume);
         this.refinerCycleVolume = 1e14 * 10**(1.1 * arcInc.savegame.talents.refinerCycleVolume);
         this.refinerOfflineVolume = 1e17 * 10**(1.1 * arcInc.savegame.talents.refinerOfflineVolume);
-        this.refinerPurity = 10 * 10**arcInc.savegame.talents.refinerPurity;
+        this.refinerPurity = 10 * 10**(0.5 * arcInc.savegame.talents.refinerPurity);
 
         this.acquisitionInterval = 1.1 - 0.1 * arcInc.savegame.talents.acquisitionInterval;
         this.acquisitionBulkBuy = 1 + arcInc.savegame.talents.acquisitionBulkBuy;
